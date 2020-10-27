e01ab752a8b7992cba0380e5e32eac953c1b66b4
Kristof Ringleff, Fooman <kristof@fooman.co.nz>
2020-10-27 16:23:53 +1300

    create an example adding load to the product

---
+++ src/Observer/Catalog/ProductLoadAfter.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Extdn\BadExample\Observer\Catalog;
+
+class ProductLoadAfter implements \Magento\Framework\Event\ObserverInterface
+{
+
+    /**
+     * Execute observer
+     *
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return void
+     */
+    public function execute(
+        \Magento\Framework\Event\Observer $observer
+    ) {
+        sleep(5);
+    }
+}
+
---
+++ src/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Extdn_BadExample', __DIR__);
+
